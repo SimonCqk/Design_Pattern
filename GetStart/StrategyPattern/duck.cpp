97a1f6b43ba616744c062d3c96c03788412614d4
Cqk <cqk0100@gmail.com>
2017-10-01 13:08:43 +0800

    correct strategy pattern .

--- GetStart/StrategyPattern/duck.cpp
+++ GetStart/StrategyPattern/duck.cpp
@@ -1,5 +1,51 @@
 #include"duck.h"
 
+void Duck::siwm()
+{
+	std::cout << "All ducks float, even decoys!" << std::endl;
+}
+
+void Duck::performFly()
+{
+	flyBehavior->fly();
+}
+
+void Duck::performQuack()
+{
+	quackBehavior->quack();
+}
+
+void FlyWithWings::fly()
+{
+	std::cout << "I'm flying!" << std::endl;
+}
+
+void FlyNoWay::fly()
+{
+	std::cout << "I can't fly!" << std::endl;
+}
+
+void Quack::quack()
+{
+	std::cout << "Quack." << std::endl;
+}
+
+void MuteQuack::quack()
+{
+	std::cout << "<< Silence >>" << std::endl;
+}
+
+void Squeak::quack()
+{
+	std::cout << "<< Squeak >>" << std::endl;
+}
+
+void MallardDuck::display()
+{
+	std::cout << "I'm a real Mallard Duck!" << std::endl;
+}
+
+
 int main() {
 	Duck *mallard = new MallardDuck;
 	mallard->performFly();
--- GetStart/StrategyPattern/duck.h
+++ GetStart/StrategyPattern/duck.h
@@ -15,7 +15,7 @@
  * we'll be hijacked by implementation, all related code
  * should be changed, which has a bad reusability.
  */
-#include<iostream>
+#include <iostream>
 
 class FlyBehavior;
 class QuackBehavior;
@@ -23,36 +23,24 @@ class QuackBehavior;
 // base abstract class - duck.
 class Duck {
 public:
-  Duck()
-      : flyBehavior(nullptr), quackBehavior(nullptr) {} // default constructor.
+  Duck() {} // default constructor.
   virtual void display() = 0;
-  virtual void siwm() {
-    std::cout << "All ducks float, even decoys!" << std::endl;
-  }
-  virtual void performFly() { flyBehavior->fly(); }
-  virtual void performQuack() { quackBehavior->quack(); }
+  virtual void siwm();
+  virtual void performFly();
+  virtual void performQuack();
 
-private:
+protected:
   // use pointer to realize polymorphic.
   FlyBehavior *flyBehavior;
   QuackBehavior *quackBehavior;
 };
 
 // define all kinds of fly behaviors.
+
 // base abstract FlyBehavior class.
 class FlyBehavior {
 public:
   virtual void fly() = 0;
-};
-
-class FlyWithWings : public FlyBehavior {
-public:
-  void fly() override { std::cout << "I'm flying!" << std::endl; }
-};
-
-class FlyNoWay : public FlyBehavior {
-public:
-  void fly() override { std::cout << "I can't fly!" << std::endl; }
 };
 
 // base abstract QuackBehavior class.
@@ -61,30 +49,40 @@ public:
   virtual void quack() = 0;
 };
 
+class FlyWithWings : public FlyBehavior {
+public:
+  void fly() override;
+};
+
+class FlyNoWay : public FlyBehavior {
+public:
+  void fly() override;
+};
+
 class Quack : public QuackBehavior {
 public:
-  void quack() override { std::cout << "Quack." << std::endl; }
+  void quack() override;
 };
 
 class MuteQuack : public QuackBehavior {
 public:
-  void quack() override { std::cout << "<< Silence >>" << std::endl; }
+  void quack() override;
 };
 
 class Squeak : public QuackBehavior {
 public:
-  void quack() override { std::cout << "<< Squeak >>" << std::endl; }
+  void quack() override;
 };
 
 // define a MallardDuck subclass.
 class MallardDuck : public Duck {
 public:
+  // since flyBehavior & quackBehavior is deribed from duck class
+  // so they can't initialzed by list-initialization but assignment is OK.
   MallardDuck() {
-	  flyBehavior = new FlyWithWings;
-	  quackBehavior = new Quack;
+    flyBehavior = new FlyWithWings;
+    quackBehavior = new Quack;
   }
-  void display() override {
-    std::cout << "I'm a real Mallard Duck!" << std::endl;
-  }
-};
 
+  void display() override;
+};
