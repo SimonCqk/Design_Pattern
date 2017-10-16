34b2c6e81a235481eb1493ff674561145763c5f9
Cqk <cqk0100@gmail.com>
2017-10-16 20:04:04 +0800

    improve.

--- ObserverPattern/test.cpp
+++ ObserverPattern/test.cpp
@@ -1,12 +1,12 @@
 #include"observer.h"
 
 int main() {
-	std::shared_ptr<WeatherData> weather_data(new WeatherData); 
-	std::shared_ptr<CurrentConditionsDisplay> current(new CurrentConditionsDisplay(weather_data));
+	std::shared_ptr<WeatherData> weather_data(new WeatherData);
+	std::shared_ptr<CurrentConditionsDisplay> current = std::make_shared<CurrentConditionsDisplay>(weather_data);
 	weather_data->registerObserver(current);
-	weather_data->setMeasurements(80,65,0.301);
+	weather_data->setMeasurements(80, 65, 0.301);
 	weather_data->setMeasurements(82, 70, 0.292);
-	std::shared_ptr<CurrentConditionsDisplay> new_curr(new CurrentConditionsDisplay(weather_data));
+	std::shared_ptr<CurrentConditionsDisplay> new_curr = std::make_shared<CurrentConditionsDisplay>(weather_data);
 	weather_data->registerObserver(new_curr);
 	weather_data->removeObserver(current);
 	weather_data->setMeasurements(78, 90, 0.292);
